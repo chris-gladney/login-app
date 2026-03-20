86619df199cebdbd8f18ad849ce069ec59ee0204
Christopher Gladney <cgladney@mba-group.com>
2026-03-20 12:08:40 +0000

    Server set up

---
+++ login-form-be/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = () => {
+    return mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+}
+
+export default connectDB;
